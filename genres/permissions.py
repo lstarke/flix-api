df6efa5dfed66b326a46c8647829bc8837c32a4e
Leandro Starke <leandro.starke@netzsch.com>
2025-04-23 05:34:06 -0300

    Configured permission in genre endpoints.

--- genres/permissions.py
+++ genres/permissions.py
@@ -1,6 +1,19 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class GenrePermission(BasePermission):
+class GenrePermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
+
+        if request.method in ['GET', 'OPTION', 'HEAD']:
+            return request.user.has_perm('genres.view_genre')
+        
+        if request.method in ['POST']:
+            return request.user.has_perm('genres.add_genre')
+        
+        if request.method in ['PATCH', 'PUT']:
+            return request.user.has_perm('genres.change_genre')
+        
+        if request.method == 'DELETE':
+            return request.user.has_perm('genres.delete_genre')
+
         return False
